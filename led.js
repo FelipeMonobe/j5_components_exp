10d5dfada9ecbc390ee1fba9690b016b90e60a08
FelipeMonobe <xinube@gmail.com>
2016-07-24 20:41:36 -0300

    all board schemas

--- led.js
+++
@@ -1,9 +0,0 @@
-var five = require("johnny-five");
-var board = new five.Board();
-
-board.on("ready", function() {
-  var led = new five.Led(13);
-
-  this.repl.inject({ led: led });
-  led.blink();
-});
