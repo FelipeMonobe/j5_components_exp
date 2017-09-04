cdeef7bd144f5dbbf262124a05bc798730a17e04
FelipeMonobe <xinube@gmail.com>
2017-09-03 21:49:58 -0300

    wt

--- tilt/sketch.js
+++ tilt/sketch.js
@@ -1,19 +1,6 @@
 const five = require('johnny-five')
 
 const board = new five.Board()
-<<<<<<< HEAD
-const pin = 'A3'
-const freq = 1 * 1000
-const main = () => {
-  const tilt = new five.Sensor({ pin, freq })
-  const printTilt = () => {
-    process.stdout.clearLine()
-    process.stdout.cursorTo(0)
-    process.stdout.write(`[${new Date().toLocaleTimeString()}] ${tilt.value}`)
-  }
-
-  tilt.on('data', printTilt)
-=======
 const printTilt = (state) => {
   const now = new Date().toLocaleTimeString()
 
@@ -30,7 +17,6 @@ const main = () => {
   const tiltPrintOnStateChange = componentPrintOnStateChange(tilt);
 
   states.forEach(tiltPrintOnStateChange)
->>>>>>> 280927b22ca7273507ea176259db2fe21468b7ad
 }
 
 board.on('ready', main)
